3d1361726b49f74578f876cad6a664829cd4a363
Nitesh Verma <nitesh.300012824041@csvtu.ac.in>
2026-03-13 22:34:21 +0530

    Time: 482 ms (99.18%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT ROUND(
+    COUNT(DISTINCT a.player_id) / COUNT(DISTINCT b.player_id), 2
+) AS fraction
+FROM
+    (SELECT player_id, MIN(event_date) AS first_login
+     FROM Activity
+     GROUP BY player_id) b
+LEFT JOIN Activity a
+ON a.player_id = b.player_id
+AND a.event_date = DATE_ADD(b.first_login, INTERVAL 1 DAY);
